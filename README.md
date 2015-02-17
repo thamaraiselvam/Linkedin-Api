b612da4ddaff9120cd4fe2b1f07f8f8730a7bc1e
Thamaraiselvam <thamaraiselvam@live.com>
2015-02-17 11:03:13 +0530

    Updated
    All required file here

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Linkedin-Api
 How to connect linkedin api with your website
+
+
+
+First go to linkedin developer portal create your app then get key and secret key for your app
+
+https://www.linkedin.com/secure/developer
+
+OAuth 1.0a Accept Redirect URL: this is your returning url after conformation(callback_url) !
+
+
+Add your key and secret key in both auth.php and demo.php
+
+
+then run auth.php . Happy coding..!
+
+Libraries
+-------------------------
+linkedin.php
+OAuth.php
+
+User's File
+-------------------------
+demo.php
+auth.php
+
+
---
+++ demo.php
@@ -0,0 +1,34 @@
+<?php
+    session_start();
+
+    $config['base_url']             =   'http://localhost/linkedin/auth.php';//example
+    $config['callback_url']         =   'http://localhost/linkedin/demo.php';//example
+      $config['linkedin_access']      =   'your key';
+    $config['linkedin_secret']      =   'your secret key';
+    include_once "linkedin.php";
+   
+    
+    # First step is to initialize with your consumer key and secret. We'll use an out-of-band oauth_callback
+    $linkedin = new LinkedIn($config['linkedin_access'], $config['linkedin_secret'], $config['callback_url'] );
+    //$linkedin->debug = true;
+
+   if (isset($_REQUEST['oauth_verifier'])){
+        $_SESSION['oauth_verifier']     = $_REQUEST['oauth_verifier'];
+
+        $linkedin->request_token    =   unserialize($_SESSION['requestToken']);
+        $linkedin->oauth_verifier   =   $_SESSION['oauth_verifier'];
+        $linkedin->getAccessToken($_REQUEST['oauth_verifier']);
+
+        $_SESSION['oauth_access_token'] = serialize($linkedin->access_token);
+        header("Location: " . $config['callback_url']);
+        exit;
+   }
+   else{
+        $linkedin->request_token    =   unserialize($_SESSION['requestToken']);
+        $linkedin->oauth_verifier   =   $_SESSION['oauth_verifier'];
+        $linkedin->access_token     =   unserialize($_SESSION['oauth_access_token']);
+   }
+
+
+  
+?>
